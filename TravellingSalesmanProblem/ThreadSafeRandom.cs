7d0af118dc6955b9a8a06d600d9ac273a776d29b
Joelius300 <bstlng321@gmail.com>
2019-12-02 13:34:47 +0100

    Use ThreadLocal<> for ThreadSafeRandom

--- TravellingSalesmanProblem/ThreadSafeRandom.cs
+++ TravellingSalesmanProblem/ThreadSafeRandom.cs
@@ -1,35 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace TravellingSalesmanProblem
 {
     public static class ThreadSafeRandom
     {
         private static readonly Random s_global = new Random();
-        [ThreadStatic]
-        private static Random? _local;
+        private static readonly ThreadLocal<Random> s_threadLocal = new ThreadLocal<Random>(GetNewRandom);
 
-        public static Random ThreadSafeInstance => AssureExisting();
+        public static Random ThreadSafeInstance => s_threadLocal.Value!; // can't be null because GetNewRandom never returns null
 
-        public static int Next() => AssureExisting().Next();
+        public static int Next() => ThreadSafeInstance.Next();
+        public static int Next(int maxValue) => ThreadSafeInstance.Next(maxValue);
+        public static int Next(int minValue, int maxValue) => ThreadSafeInstance.Next(minValue, maxValue);
+        public static double NextDouble() => ThreadSafeInstance.NextDouble();
 
-        public static int Next(int maxValue) => AssureExisting().Next(maxValue);
-
-        public static int Next(int minValue, int maxValue) => AssureExisting().Next(minValue, maxValue);
-
-        public static double NextDouble() => AssureExisting().NextDouble();
-
-        private static Random AssureExisting()
+        private static Random GetNewRandom()
         {
-            if (_local == null)
+            int seed;
+            lock (s_global)
             {
-                int seed;
-                lock (s_global) seed = s_global.Next();
-                _local = new Random(seed);
+                seed = s_global.Next();
             }
-
-            return _local;
+            return new Random(seed);
         }
     }
 }
